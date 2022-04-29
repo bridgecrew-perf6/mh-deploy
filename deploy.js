471d442e8a4a3f671e2277d212e70706431c9427
misted <mihu0915@163.com>
2022-04-29 13:38:21 +0800

    webhooks自动部署

---
+++ deploy.js
@@ -0,0 +1,46 @@
+const http = require('http')
+const createHandler = require('github-webhook-handler')
+const handler = createHandler({ path: '/webhook', secret: 'tj991118.' })
+
+const RunCmd = (cmd, args, cb) => {
+  const spawn = require('child_process').spawn
+  const child = spawn(cmd, args)
+  let result = ''
+  child.stdout.on('data', data => {
+    result += data.toString()
+  })
+  child.stdout.on('end', () => {
+    cb(result)
+  })
+}
+
+http
+  .createServer(function (req, res) {
+    handler(req, res, function (err) {
+      res.statusCode = 404
+      res.end('no such location')
+    })
+  })
+  .listen(8080)
+
+handler.on('error', function (err) {
+  console.error('Error:', err.message)
+})
+
+handler.on('push', function (event) {
+  console.log(
+    'Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref
+  )
+})
+
+handler.on('issues', function (event) {
+  console.log(
+    'Received an issue event for %s action=%s: #%d %s',
+    event.payload.repository.name,
+    event.payload.action,
+    event.payload.issue.number,
+    event.payload.issue.title
+  )
+})
